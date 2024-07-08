15cec51e99b764f916d13b1fab77eb07ff012474
Phooba <ich@shneeble.com>
2024-07-08 14:29:51 +0200

    added comments

--- core/exceptions.h
+++ core/exceptions.h
@@ -1,6 +1,16 @@
 #include <exception>
 #include <string>
 
+/**
+ * Exceptions are used to exit the game loop (without numerous nested "if( player->isDead() ) return" - calls)
+ */
+
+/**
+ * @brief can be thrown, when the player died.
+ *        exit the game loop, trigger death effects
+ * @param[in] what a death text to be displayed.
+ * @remark cbattle throws this exception if the player looses a battle
+ */
 class CPlayerDiedException : public std::exception
 {
 public:
@@ -17,6 +27,10 @@ private:
     }
 };
 
+/**
+ * @brief can be thrown to end the game.
+ *        no save - no mercy when this is thrown from inside the game loop. the game ends
+ */
 class CGameOverException : public std::exception
 {
     virtual const char* what() const throw()
