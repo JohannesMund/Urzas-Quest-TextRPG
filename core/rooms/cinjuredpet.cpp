ebfaa1289fa6bad2bd7416eb163a3a8ad87302b0
Johannes Mund <johannes.mund@amdt.com>
2026-02-16 16:06:11 +0100

    fixed trs, added translations

--- core/rooms/cinjuredpet.cpp
+++ core/rooms/cinjuredpet.cpp
@@ -21,22 +21,22 @@ void CInjuredPet::execute()
     if (CGameManagement::getCompanionInstance()->hasCompanion())
     {
         Console::br();
-        Console::printLn(
-            tr("With a smile, you remember how you found {} here", CGameManagement::getCompanionInstance()->name()));
+        Console::printLn(coreTr("With a smile, you remember how you found {} here",
+                                CGameManagement::getCompanionInstance()->name()));
         return;
     }
 
     if (_petIsDead)
     {
         Console::br();
-        Console::printLn(tr("You see the corpse of the {} you left here dying. You feel pitty for the little guy.",
-                            CGameManagement::getCompanionInstance()->name()));
+        Console::printLn(coreTr("You see the corpse of the {} you left here dying. You feel pitty for the little guy.",
+                                CGameManagement::getCompanionInstance()->name()));
         return;
     }
 
     Console::br();
-    Console::printLn(tr("A suspicious noise is coming out of one of the bushes, you should have a look. Or maybe, "
-                        "you better be carful and ignore it."));
+    Console::printLn(coreTr("A suspicious noise is coming out of one of the bushes, you should have a look. Or maybe, "
+                            "you better be carful and ignore it."));
     Console::hr();
 
     CMenu menu;
@@ -47,15 +47,16 @@ void CInjuredPet::execute()
     auto input = menu.execute();
     if (input == goAction)
     {
-        Console::printLn(tr("Better safe than sorry! You decide to ignore the pittyful screams and leave."));
-        Console::printLn(tr("The screams vanish shortly after."));
+        Console::printLn(coreTr("Better safe than sorry! You decide to ignore the pittyful screams and leave."));
+        Console::printLn(coreTr("The screams vanish shortly after."));
         _petIsDead = true;
         return;
     }
 
     Console::br();
-    Console::printLn(tr("You follow the sounds, and find a tiny {} lying beneath a bush, desperately looking for help",
-                        CGameManagement::getCompanionInstance()->name()));
+    Console::printLn(
+        coreTr("You follow the sounds, and find a tiny {} lying beneath a bush, desperately looking for help",
+               CGameManagement::getCompanionInstance()->name()));
     Console::hr();
 
     menu.clear();
@@ -64,19 +65,19 @@ void CInjuredPet::execute()
     menu.addMenuGroup({helpAction, goAction});
     if (menu.execute() == goAction)
     {
-        Console::printLn(tr("Better safe than sorry! You decide to ignore the pittyful little animal and leave."));
-        Console::printLn(tr("The screams vanish shortly after."));
+        Console::printLn(coreTr("Better safe than sorry! You decide to ignore the pittyful little animal and leave."));
+        Console::printLn(coreTr("The screams vanish shortly after."));
         _petIsDead = true;
         return;
     }
 
     Console::br();
-    Console::printLn(tr("You decide to help the {}. a little water, a bandage for the broken limb and some "
-                        "herbs and soon the little fellow is back on track.",
-                        CGameManagement::getCompanionInstance()->name()));
+    Console::printLn(coreTr("You decide to help the {}. a little water, a bandage for the broken limb and some "
+                            "herbs and soon the little fellow is back on track.",
+                            CGameManagement::getCompanionInstance()->name()));
     Console::printLn(
-        tr("As soon as you leave, you realize, that {} follows you. Looks like, you have found a new friend.",
-           CGameManagement::getCompanionInstance()->name()));
+        coreTr("As soon as you leave, you realize, that {} follows you. Looks like, you have found a new friend.",
+               CGameManagement::getCompanionInstance()->name()));
     CGameManagement::getCompanionInstance()->evolve();
 
     _showInFogOfWar = false;
